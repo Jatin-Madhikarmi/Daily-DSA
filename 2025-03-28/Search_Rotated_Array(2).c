c280316041bb931906d6e8469c90d42ee1d1ca22
Jatin-Madhikarmi <jatinmadhikarmi8@gmail.com>
2025-03-28 15:28:14 +0545

    Search in a Rotated Array 2.

---
+++ 2025-03-28/Search_Rotated_Array(2).c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+int Search(int [],int,int);
+int main()
+{
+    printf("Searching in a rotated array.\n");
+    int arr[]={1,1,1,1,1,1,1,1,2,1,1,1};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int target=2;
+    int result=Search(arr,n,target);
+    if(result==1) printf("The target %d is present in the given data set.\n",target);
+    else printf("The target %d is not found in the given data set.\n",target);
+
+}
+
+int Search(int arr[], int n, int target)
+{
+    int left=0;
+    int right=n-1;
+    while(left<=right)
+    {
+        int middle=left+(right-left)/2;
+
+        if(arr[middle]=target) return 1;
+
+        if(arr[middle]== arr[left])
+        {
+            left++;
+            continue;
+        }
+
+
+        if(arr[left] <= arr[middle])
+        {
+            if(arr[left] <= target && target<arr[middle]) right=middle-1;
+            else left=middle+1;
+        }
+        else
+        {
+            if(arr[middle] < target && target<=arr[right]) left=middle+1;
+            else right=middle-1;
+        }
+
+    }
+    return 0;
+}
